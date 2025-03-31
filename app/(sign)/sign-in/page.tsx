dd4adf2165ac1b6d0c87f6ec2955af6503be4e9c
CanioV <2756432588@qq.com>
2025-03-31 22:48:16 +0800

    init

---
+++ app/(sign)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+export default function SignIn() {
+  return (
+    <div className=''>
+      <h1 className='text-4xl font-bold'>欢迎来到 EduPath</h1>
+      <p className='mt-2 text-lg'>请登录以继续</p>
+    </div>
+  )
+}
